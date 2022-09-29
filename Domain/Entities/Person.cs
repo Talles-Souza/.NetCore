e0208bc29e938368514cd215cca54652253a41b3
Talles Dias <tallesdelfino1@gmail.com>
2022-09-29 16:15:27 -0300

    primeiro commit

---
+++ Domain/Entities/Person.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Entities
+{
+    internal sealed class Person
+    {
+        public Person(string name, string document, string phone)
+        {
+            Name = name;
+            Document = document;
+            Phone = phone;
+        }
+
+        public int Id { get; private set; }   
+        public string Name { get; private set; }    
+        public string Document { get; private set; }
+        public string Phone {get; private set; }
+
+
+    }
+}
