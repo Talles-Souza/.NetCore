66dc473c70a36866642c7d6ff23ae9fcfde49305
Talles Dias <tallesdelfino1@gmail.com>
2022-09-29 17:31:23 -0300

    começando a implementar o domain

--- Domain/Entities/Person.cs
+++ Domain/Entities/Person.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Domain.Validations;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,18 +9,31 @@ namespace Domain.Entities
 {
     internal sealed class Person
     {
-        public Person(string name, string document, string phone)
-        {
-            Name = name;
-            Document = document;
-            Phone = phone;
-        }
-
         public int Id { get; private set; }   
         public string Name { get; private set; }    
         public string Document { get; private set; }
         public string Phone {get; private set; }
 
+        public Person(string name, string document, string phone)
+        {
+            Validation(name,document,phone);
+        }
+        public Person(int id,string name, string document, string phone)
+        {
+            DomainValidationException.When(id < 0, "Id must be greater than zero");
+            Validation(name,document,phone);
+        }
+
+        private void Validation(string name, string document, string phone)
+        {
+            DomainValidationException.When(string.IsNullOrEmpty(name), "Name must be entered correctly");
+            DomainValidationException.When(string.IsNullOrEmpty(document), "Document must be correctly informed");
+            DomainValidationException.When(string.IsNullOrEmpty(phone), "Phone must be informed correctly");
+
+            Name = name;
+            Document = document;
+            Phone = phone;  
+        }
 
     }
 }
---
+++ Domain/Validations/DomainValidationException.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Validations
+{
+    //Classe de validação genérica
+    internal class DomainValidationException:Exception
+    {
+        public DomainValidationException(string error):base(error)
+        {
+
+        }
+        public static void When(bool hasError,string message)
+        {
+            if (hasError)
+                throw new DomainValidationException(message);
+        }
+    }
+}
