a51af6bc12e07706986f4a6c7779ffe302508c9c
Talles Dias <tallesdelfino1@gmail.com>
2022-09-30 19:47:16 -0300

    map de product

---
+++ Data/Maps/ProductMap.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Data.Maps
+{
+    public class ProductMap : IEntityTypeConfiguration<Product>
+    {
+    }
+}
