baca5f612da924f93a01fe015faf3419be90f6ee
Talles Dias <tallesdelfino1@gmail.com>
2022-10-03 12:25:09 -0300

    fazendo outros mapeamentos

--- Data/Maps/ProductMap.cs
+++ Data/Maps/ProductMap.cs
@@ -1,13 +1,24 @@
-﻿using Microsoft.EntityFrameworkCore;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
 
 namespace Data.Maps
 {
     public class ProductMap : IEntityTypeConfiguration<Product>
     {
+        public void Configure(EntityTypeBuilder<Product> builder)
+        {
+            builder.ToTable("Product");
+            builder.HasKey(x => x.Id);
+            
+            builder.Property(x => x.Id).HasColumnName("IdProduct").UseIdentityColumn();  
+            builder.Property(x => x.Name).HasColumnName("Name");    
+            builder.Property(x => x.Cod).HasColumnName("Cod");    
+            builder.Property(x => x.Price).HasColumnName("Price");
+            builder.HasMany(x => x.Purchases).WithOne(prop => prop.Product).HasForeignKey(x => x.ProductId);
+
+
+        }
     }
 }
+
---
+++ Data/Maps/PurchaseMap.cs
@@ -0,0 +1,24 @@
+﻿using Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Data.Maps
+{
+
+    public class PurchaseMap : IEntityTypeConfiguration<Purchases>
+    {
+    
+
+        public void Configure(EntityTypeBuilder<Purchases> builder)
+        {
+
+            builder.ToTable("Purchases");
+            builder.HasKey(x => x.Id);
+
+            builder.Property(x => x.Id).HasColumnName("IdPuerchase").UseIdentityColumn();
+            builder.Property(x => x.PersonId).HasColumnName("IdPerson");
+            builder.Property(x => x.ProductId).HasColumnName("IdProduct");
+            builder.Property(x => x.Date).HasColumnName("DataPurchase");
+        }
+    }
+}
