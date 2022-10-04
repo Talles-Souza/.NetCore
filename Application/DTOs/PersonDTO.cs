15f02e805c79f08f694ebcd9624a2f116dbaf6ef
Talles Dias <tallesdelfino1@gmail.com>
2022-10-04 15:38:03 -0300

    fazendo o persondtovalidation

---
+++ Application/DTOs/PersonDTO.cs
@@ -0,0 +1,17 @@
+﻿using Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.DTOs
+{
+    public class PersonDTO
+    {
+        public int Id { get; private set; }
+        public string Name { get; private set; }
+        public string Document { get; private set; }
+        public string Phone { get; private set; }
+    }
+}
---
+++ Application/DTOs/PersonDTOValidator.cs
@@ -0,0 +1,16 @@
+﻿using FluentValidation;
+
+namespace Application.DTOs
+{
+    public class PersonDTOValidator : AbstractValidator<PersonDTO>  
+    {
+        public PersonDTOValidator()
+        {
+            RuleFor(x => x.Name).NotEmpty().NotNull().WithMessage("Name must be informed");
+            RuleFor(x => x.Document).NotEmpty().NotNull().WithMessage("Document must be informed");
+            RuleFor(x => x.Phone).NotEmpty().NotNull().WithMessage("Phone must be informed");
+            
+
+        }
+    }
+}
